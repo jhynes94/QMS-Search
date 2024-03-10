fc602136283607e2ca08454e4ca64c0a6543c25a
Justin Hynes-Bruell <jbruell94@gmail.com>
2024-03-10 13:32:30 -0700

    Added Build Folder ti script

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # Generated from https://nodejs.org/api/single-executable-applications.html
 node --experimental-sea-config package.json
 
-cp $(command -v node) qms
+cp $(command -v node) ./build/qms
 # If System is Windows, run this:
 # node -e "require('fs').copyFileSync(process.execPath, 'hello.exe')"
 
-codesign --remove-signature qms
+codesign --remove-signature ./build/qms
 
-npx postject qms NODE_SEA_BLOB QMS-Search.blob \
+npx postject ./build/qms NODE_SEA_BLOB QMS-Search.blob \
     --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
     --macho-segment-name QMS-Search
 
